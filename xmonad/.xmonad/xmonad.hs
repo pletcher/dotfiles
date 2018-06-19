5573cc4a05816ffa267abe54d997d343ac764868
Charles Pletcher <cpletcher2+goldman@gmail.com>
2018-06-19 14:19:06 -0400

    Update ranger and xmonad

--- xmonad/.xmonad/xmonad.hs
+++
@@ -1,9 +0,0 @@
-import XMonad
-import XMonad.Config.Kde
-import XMonad.Layout.Spacing
-
-main = xmonad $ def
-       { borderWidth        = 2
-       , terminal           = "konsole"
-       , normalBorderColor  = "#cccccc"
-       , focusedBorderColor = "#cd8b00" }
