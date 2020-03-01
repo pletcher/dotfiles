faa50d99c642e2994865ec884d78023242a5f550
Charles Pletcher <c@plet.ch>
2020-03-01 15:19:14 -0500

    Update fish md2docx

--- fish/.config/fish/functions/md2docx.fish
+++ fish/.config/fish/functions/md2docx.fish
@@ -1,3 +1,3 @@
 function md2docx
-  pandoc --filter pandoc-citeproc --csl /home/pletcher/code/csls/chicago-author-date.csl -i $argv[1] -o $argv[1]-pandoc.docx
+  pandoc --filter pandoc-citeproc --csl /home/pletcher/code/csls/chicago-author-date.csl --reference-docx /home/pletcher/.local/share/pandoc/reference.doc -i $argv[1] -o $argv[1]-pandoc.docx
 end
